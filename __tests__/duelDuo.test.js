d94ba82135cdbf9d510fe563f3bbc51ee52ee32f
diabm1 <diabm1855@gmail.com>
2023-05-15 15:08:10 -0400

    complete part 4 automated tests

--- __tests__/duelDuo.test.js
+++ __tests__/duelDuo.test.js
@@ -10,9 +10,51 @@ afterEach(async () => {
   await driver.quit();
 });
 
+/*
+    Check that clicking the Draw button displays the div with id = “choices”
+
+    Check that clicking an “Add to Duo” button displays the div with id = “player-duo”
+
+    Check that when a bot is “Removed from Duo”, that it goes back to “choices”
+*/
+
 describe("Duel Duo tests", () => {
   test("page loads with title", async () => {
     await driver.get("http://localhost:8000");
     await driver.wait(until.titleIs("Duel Duo"), 1000);
   });
+
+  test("clicking the Draw button displays the div with id = 'choices'", async () => {
+    await driver.get("http://localhost:8000");
+    let drawButton = await driver.findElement(By.id("draw"));
+    await drawButton.click();
+    // wait until the choicesDiv contains child divs (i.e., the bot cards)
+    await driver.wait(until.elementLocated(By.css("#choices .bot-card")), 1000);
+  });  
+    
+  test("clicking an 'Add to Duo' button displays the div with id = 'player-duo'", async () => {
+    await driver.get("http://localhost:8000");
+    let drawButton = await driver.findElement(By.id("draw"));
+    await drawButton.click();
+    // wait until the choicesDiv contains child divs and then click the first "Add to Duo" button
+    let addButton = await driver.wait(until.elementLocated(By.css("#choices .bot-card .bot-btn")), 1000);
+    await addButton.click();
+    // wait until the playerDuoDiv contains child divs (i.e., the bot cards)
+    await driver.wait(until.elementLocated(By.css("#player-duo .bot-card")), 1000);
+  });
+
+  test("when a bot is 'Removed from Duo', it goes back to 'choices'", async () => {
+    await driver.get("http://localhost:8000");
+    let drawButton = await driver.findElement(By.id("draw"));
+    await drawButton.click();
+    // wait until the choicesDiv contains child divs and then click the first "Add to Duo" button
+    let addButton = await driver.wait(until.elementLocated(By.css("#choices .bot-card .bot-btn")), 1000);
+    await addButton.click();
+    // wait until the playerDuoDiv contains child divs and then click the first "Remove from Duo" button
+    let removeButton = await driver.wait(until.elementLocated(By.css("#player-duo .bot-card .bot-btn")), 1000);
+    await removeButton.click();
+    // wait until the choicesDiv contains child divs (i.e., the bot cards)
+    await driver.wait(until.elementLocated(By.css("#choices .bot-card")), 1000);
+  });
+  
 });
